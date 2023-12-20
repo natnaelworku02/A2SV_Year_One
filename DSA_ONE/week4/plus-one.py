ecf9040d2cfd0ab83344cbc9d4a35b50e5ba6e1e
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2023-12-20 18:16:22 +0300

    Added solution for Plus One

---
+++ DSA_ONE/week4/plus-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        length=len(digits)-1
+        while digits[length]==9:
+            digits[length]=0
+            length-=1
+        if length<0:
+            digits=[1]+digits
+        else:
+            digits[length]+=1
+
+        return digits
+
+                
+
+                
+
+
+        
