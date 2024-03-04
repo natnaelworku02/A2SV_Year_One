d937ff968169ab6df2357328756747ccf9f598b2
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-03-04 10:57:46 +0300

    Add solution for Combinations

---
+++ DSA_ONE/week9/leetcode/combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        ans = []
+        path = []
+        def comb(fir_num,path):
+            if len(path) == k:
+                ans.append(path.copy())
+            for i in range(fir_num,n+1):
+                path.append(i)
+                # print(path,"i")
+                
+                comb(i + 1, path)
+                path.pop()
+        comb(1,path)
+        return ans
+                
+
+
