a95982344060b69d7be669d1e3e3568417b5d04c
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-04 10:09:11 +0300

    Time: 33 ms (71.93%) | Memory: 16.5 MB (90.66%) - LeetSync

--- Phase2/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ Phase2/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,37 +1,12 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
         
-        st = []
-        ans = []
-        temp = 0
-        close = 0
-        for i,ch in enumerate(s):
-            if ch == "(":
-                st.append([i,ch])
-                if temp:
-                    ans.append([close,temp])
-                temp = 0
-                close = -1
-            elif ch == ')' and st:
-                ind,_ = st.pop() 
-                # print(i)
-                for idx,val in enumerate(ans):
-                    # print(ans)
-                    # print(ind,val[0],i)
-                    # print("_______")
-                    if ind < val[0]:
-                        ans[idx][1] += 1
-                temp += 1
-                close = i 
-            elif ch == ")" and not st:
-                return -1
-            
-        ans.append([close,temp])
-        # print(ans)
-        if st:
-            return -1
+        count = ans = 0
 
-        res = 0
-        for i in range(len(ans)):
-            res = max(res,ans[i][1])
-        return res
+        for i in s:
+            if i == "(":
+                count += 1
+                ans = max(ans,count)
+            elif i == ")":
+                count -= 1
+        return ans
