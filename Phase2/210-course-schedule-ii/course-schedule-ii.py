94ac7d4e46b10d57b84b86bd9ac931f3ebfe7732
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-18 18:39:45 +0300

    Time: 96 ms (28.78%) | Memory: 18.4 MB (45.16%) - LeetSync

---
+++ Phase2/210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        indeg = [0] * numCourses
+        adjlist = defaultdict(list)
+
+        for u ,v in prerequisites:
+            adjlist[v].append(u)
+            indeg[u] += 1
+        
+        queue = deque()
+        for i , count in enumerate(indeg):
+            if count == 0:
+                queue.append(i)
+        # print(queue)
+        ans = []
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                ans.append(node)
+                for ind in adjlist[node]:
+                    indeg[ind] -= 1
+                    if indeg[ind] == 0:
+                        queue.append(ind)
+        for count in indeg:
+            if count > 0:
+                return []
+        return ans
