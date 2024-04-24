042dec198fae567e474dfecd25d9f72051e85a98
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-24 23:43:51 +0300

    Time: 38 ms (29.68%) | Memory: 16.6 MB (31.23%) - LeetSync

---
+++ Phase2/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    @cache
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n== 2:
+            return 1
+        
+        val = self.tribonacci(n- 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+        return val
