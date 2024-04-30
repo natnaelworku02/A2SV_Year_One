b16491361f90204797ef193071fd5b67ddab691a
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-30 18:23:49 +0300

    Time: 56 ms (85.22%) | Memory: 23.2 MB (48.89%) - LeetSync

--- Phase2/338-counting-bits/counting-bits.py
+++ Phase2/338-counting-bits/counting-bits.py
@@ -1,22 +1,27 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def bit(n):
-            res = []
-            while n >1:
-                div = n // 2
-                mod = n % 2
-                res.append(str(mod))
-                n = div
-                if div <=1:
-                    break
-            res.append(str(n))
-            res = res[::-1]
-            return ''.join(res)
+        # def bit(n):
+        #     res = []
+        #     while n >1:
+        #         div = n // 2
+        #         mod = n % 2
+        #         res.append(str(mod))
+        #         n = div
+        #     res.append(str(n))
+        #     res = res[::-1]
+        #     return ''.join(res)
 
         ans = []
-
+        # print(bit(100000))
         for i in range(n+1):
-            val = bit(i)
+            if i == 0:
+                ans.append(0)
+                continue
+            # val = bit(i)
             # print(val)
-            ans.append(val.count("1"))
+            if i % 2:
+                ans.append(ans[-1]+1)
+            else:
+                ans.append(ans[i//2])
+            # ans.append(val.count("1"))
         return ans
