bedf6786b705b378da6e50ff83ab7b950c3f2cc3
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-30 19:19:41 +0300

    Time: 108 ms (74.44%) | Memory: 17.8 MB (30.98%) - LeetSync

--- Phase2/268-missing-number/missing-number.py
+++ Phase2/268-missing-number/missing-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)):
-            if i ^ nums[i] :
-                return i
-        return len(nums)
         
+        val = 0
+        num = nums[0]
+
+        for i in range(1,len(nums)):
+            num ^= nums[i]
+            val ^= i
+        val ^= len(nums)
+        return val ^ num
+
         
