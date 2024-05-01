c1aa207161524e5fc4697bdae0638ea1f8d784c8
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-01 09:52:22 +0300

    Time: 581 ms (59.69%) | Memory: 31 MB (73.80%) - LeetSync

--- Phase2/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ Phase2/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -3,13 +3,7 @@ class Solution:
         final_xor = 0
         for n in nums:
             final_xor = final_xor ^ n
+        
+        
 
-        count = 0
-        while k or final_xor:
-            if (k % 2) != (final_xor % 2):
-                count += 1
-
-            k //= 2
-            final_xor //= 2
-
-        return count
+        return bin(final_xor ^ k).count("1")
