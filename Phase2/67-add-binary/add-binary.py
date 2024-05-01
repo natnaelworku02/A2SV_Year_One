7db1b97b1476356c7a30d8746c2e57207d2f0541
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-01 09:25:55 +0300

    Time: 39 ms (44.58%) | Memory: 16.7 MB (35.04%) - LeetSync

--- Phase2/67-add-binary/add-binary.py
+++ Phase2/67-add-binary/add-binary.py
@@ -1,3 +1,34 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2) + int(b,2))[2:]
+        a = list(map(int,a))[::-1]
+        b = list(map(int,b))[::-1]
+        carry = 0
+
+        if len(a ) > len(b):
+            b.extend([0 for _ in range(len(a) - len(b))])
+        else:
+            a.extend([0 for _ in range(len(b) - len(a))])
+        
+        # print(a)
+        # print(b)
+        # return ""
+        ans = []
+        for i in range(len(a)):
+            val = a[i] + b[i] + carry
+            if val== 2:
+                ans.append(0)
+                carry = 1
+            elif val == 3:
+                ans.append(1)
+                carry = 1
+            else:
+                ans.append(val)
+                carry = 0
+        if carry: ans.append(carry)
+
+        return ''.join(map(str,ans[::-1]))
+
+
+
+
+        
