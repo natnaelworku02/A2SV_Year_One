813f51bb9e224ff1566a47bada8041c8b8ff6f52
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-16 18:15:48 +0300

    Time: 42 ms (14.35%) | Memory: 16.4 MB (86.32%) - LeetSync

--- Phase2/62-unique-paths/unique-paths.py
+++ Phase2/62-unique-paths/unique-paths.py
@@ -1,31 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = []
-        for i in range(m):
-            temp = []
-            for j in range(n):
-                temp.append(1)
-            memo.append(temp)
-
-        # memo[0][0] = 1
-        # print(memo[0][1])
-        for i in range(m):
+        memo = [1  for _ in range(n)]
+        # print(memo)
+        for i in range(1,m):
+            cur =  [0  for _ in range(n)]
             for j in  range(n):
-                val = 0
-                if i ==0 and j == 0:
-                    continue
-                if i - 1 > -1:
-                    # print("hi",i,j)
-                    val += memo[i-1][j]
-                    # print(memo[i-1][j])
+                cur[j] += memo[j]
+                # print(cur[j],memo[j])
                 if j - 1 > -1:
-                    # print("bye",i,j)
-                    # print(memo[i][j - 1])
-
-                    val += memo[i][j - 1]
+                    cur[j] += cur[j-1]
+            
+            # print(cur)
+            
+            memo = cur
                 
-                memo[i][j] = val
-                # print(memo[i][j],val)
-                # print("______________________")
+
         
-        return memo[m-1][n-1]
+        return memo[-1]
