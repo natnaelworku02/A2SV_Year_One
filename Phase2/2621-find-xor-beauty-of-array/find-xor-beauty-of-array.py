c0ab4007a6fd1e65759c4903ae71e96b60600090
Natnael Worku <104618033+natnaelworku02@users.noreply.github.com>
2024-10-03 11:15:32 +0300

    Time: 279 ms (23.74%) | Memory: 31.1 MB (90.91%) - LeetSync

---
+++ Phase2/2621-find-xor-beauty-of-array/find-xor-beauty-of-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorBeauty(self, nums: List[int]) -> int:
+        
+        ans = nums[0]
+
+        for i in range(1,len(nums)):
+            ans ^= nums[i]
+        
+        return ans
