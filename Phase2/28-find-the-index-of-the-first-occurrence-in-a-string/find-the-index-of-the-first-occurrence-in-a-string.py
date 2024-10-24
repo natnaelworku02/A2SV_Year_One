66c534991225960f0740ae8a17dd36dbe86aeef2
Natnael Worku <104618033+natnaelworku02@users.noreply.github.com>
2024-10-24 18:11:34 +0300

    Time: 4 ms (93.21%) | Memory: 16.6 MB (9.31%) - LeetSync

--- Phase2/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ Phase2/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,16 +1,31 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
         
-        for i in range(len(haystack)):
-            l = i
-            r = 0
-            count = 0
-            while r < len(needle) and l < len(haystack) and haystack[l] == needle[r]:
-                l += 1
-                r += 1
-                count += 1
-            
-            if count == len(needle):
-                return i
+        val  = 0
+        comp = 0
+        alpha = 26
+        leng = len(needle) - 1
+
+        for i in range(leng , -1 ,- 1):
+            # print(haystack[i])
+            val += alpha**(leng-i) * (ord(haystack[i])  - 96)
+            comp += alpha**(leng-i) * (ord(needle[i] ) - 96)
+        # print(val,comp)
+        if val == comp:
+            return 0
+        
+        for i in range(leng + 1, len(haystack)):
+            # print(i - (leng + 1),i)
+            val -= (ord(haystack[i - (leng + 1)]) - 96 ) * alpha**(leng)
+
+            val *= alpha
+
+            val +=  (ord(haystack[i]) - 96 )
+
+
+            if val == comp:
+                return i - leng
         
         return -1
